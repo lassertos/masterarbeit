7c641247ea567712f06ce1948c343eafdf458c6b
lassertos <pierre.helbing@t-online.de>
2024-05-06 09:14:46 +0200

    update results

--- latex/arbeit/scripts/literature-search/results/merge.js
+++ latex/arbeit/scripts/literature-search/results/merge.js
@@ -12,19 +12,43 @@ const data = {
 const merged = [];
 
 for (const entry of data.acm) {
-  merged.push({ title: entry.title, abstract: entry.abstract });
+  merged.push({
+    title: entry.title,
+    abstract: entry.abstract,
+    keywords: entry.keyword?.split(", ") ?? [],
+    released: parseInt(entry["issued"]["date-parts"][0][0]),
+    link: entry.URL,
+  });
 }
 
 for (const entry of data.ieee) {
-  merged.push({ title: entry["Document Title"], abstract: entry.Abstract });
+  merged.push({
+    title: entry["Document Title"],
+    abstract: entry.Abstract,
+    keywords: entry["Author Keywords"]?.split(";") ?? [],
+    released: parseInt(entry["Publication Year"]),
+    link: entry["PDF Link"],
+  });
 }
 
 for (const entry of data.scopus) {
-  merged.push({ title: entry.Title, abstract: entry.Abstract });
+  merged.push({
+    title: entry.Title,
+    abstract: entry.Abstract,
+    keywords: entry["Author Keywords"]?.split("; ") ?? [],
+    released: parseInt(entry.Year),
+    link: entry["Link"],
+  });
 }
 
 for (const entry of data.wos) {
-  merged.push({ title: entry.title, abstract: entry.abstract });
+  merged.push({
+    title: entry.title,
+    abstract: entry.abstract,
+    keywords: entry.keyword?.split("; ") ?? [],
+    released: parseInt(entry["issued"]["date-parts"][0][0]),
+    link: "",
+  });
 }
 
 const unique = merged.filter(
@@ -51,15 +75,81 @@ const microcontroller = unique.filter((e) =>
 );
 const crdt = unique.filter((e) => e.abstract?.toLowerCase().includes("crdt"));
 
-console.log("merged all:", merged.length);
-console.log("merged unique:", unique.length);
-console.log("merged duplicates:", duplicates.length);
-console.log("merged without abstract:", noAbstract.length);
-console.log("merged debug:", debug.length);
-console.log("merged collaborative:", collaborative.length);
-console.log("merged compile:", compile.length);
-console.log("merged language server:", languageServer.length);
-console.log("merged microcontroller:", microcontroller.length);
-console.log("merged crdt:", crdt.length);
+// console.log("merged all:", merged.length);
+// console.log("merged unique:", unique.length);
+// console.log("merged duplicates:", duplicates.length);
+// console.log("merged without abstract:", noAbstract.length);
+// console.log("merged debug:", debug.length);
+// console.log("merged collaborative:", collaborative.length);
+// console.log("merged compile:", compile.length);
+// console.log("merged language server:", languageServer.length);
+// console.log("merged microcontroller:", microcontroller.length);
+// console.log("merged crdt:", crdt.length);
 
-fs.writeFileSync("merged/merged.json", JSON.stringify(unique, null, 4));
+fs.writeFileSync(
+  "merged/merged.json",
+  JSON.stringify(
+    unique
+      .sort((a, b) => {
+        if (a.released > b.released) return -1;
+        if (a.released === b.released) return 0;
+        if (a.released < b.released) return 1;
+      })
+      .filter((entry) => {
+        const { title, abstract, keywords } = entry;
+        let valid = false;
+
+        if (
+          title.includes("IDE") ||
+          title.toLowerCase().includes("programming environment") ||
+          title.toLowerCase().includes("programming tool") ||
+          title.toLowerCase().includes("development environment") ||
+          title.toLowerCase().includes("development tool") ||
+          title.toLowerCase().includes("code editor")
+        )
+          valid = true;
+
+        if (
+          keywords.find((keyword) => keyword.toLowerCase() === "ide") ||
+          keywords.find((keyword) =>
+            keyword.toLowerCase().includes("development environment")
+          ) ||
+          keywords.find((keyword) =>
+            keyword.toLowerCase().includes("development tool")
+          ) ||
+          keywords.find((keyword) =>
+            keyword.toLowerCase().includes("programming environment")
+          ) ||
+          keywords.find((keyword) =>
+            keyword.toLowerCase().includes("programming tool")
+          ) ||
+          keywords.find((keyword) =>
+            keyword.toLowerCase().includes("code editor")
+          )
+        )
+          valid = true;
+
+        // if (
+        //   (abstract
+        //     ?.toLowerCase()
+        //     .includes("integrated development environment") ||
+        //     abstract?.includes("IDE") ||
+        //     abstract?.toLowerCase().includes("programming environment") ||
+        //     abstract?.toLowerCase().includes("programming tool") ||
+        //     abstract?.toLowerCase().includes("development tool")) &&
+        //   (abstract?.toLowerCase().includes("student") ||
+        //     abstract?.toLowerCase().includes("university") ||
+        //     abstract?.toLowerCase().includes("college") ||
+        //     abstract?.toLowerCase().includes("education"))
+        // )
+        //   valid = true;
+
+        return valid;
+      })
+      .map((entry) => {
+        return { ...entry, keywords: entry.keywords.join("; ") };
+      }),
+    null,
+    4
+  )
+);
