f9f6881f62cd3efe887f550f5f90252174c589d9
Pierre Helbing <pierre.helbing@t-online.de>
2024-06-10 07:56:46 +0200

    sync

--- code/vscode-extensions/git-in-browser/src/web/extension.ts
+++ code/vscode-extensions/git-in-browser/src/web/extension.ts
@@ -1,35 +1,13 @@
 import * as vscode from "vscode";
-import path from "path";
-import { GitBaseExtension } from "./git-base";
-// import git from "isomorphic-git";
+import git from "isomorphic-git";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log(
     'Congratulations, your extension "git-in-browser" is now active in the web extension host!'
   );
 
-  const gitBaseExtension =
-    vscode.extensions.getExtension<GitBaseExtension>(
-      "vscode.git-base"
-    )?.exports;
-  const git = gitBaseExtension?.getAPI(1);
-  git?.registerRemoteSourceProvider({
-    getRemoteSources(query) {
-      return [
-        {
-          name: "Test Remote Source",
-          url: "https://github.com/Cross-Lab-Project/crosslab",
-        },
-      ];
-    },
-    getBranches(url) {
-      return ["main"];
-    },
-    name: "Test Remote Source Provider",
-  });
-  git?.pickRemoteSource({ branch: true }).then((result) => {
-    console.log(result);
-  });
+  const gitSCM = vscode.scm.createSourceControl("git", "git");
+  const resourceGroup = gitSCM.createResourceGroup("index", "index");
 }
 
 export function deactivate() {}
